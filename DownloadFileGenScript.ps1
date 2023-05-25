ac8cab0d272178d096f730694793e2f17c00fdb8
Chris de Bruijn <xevichris@gmail.com>
2023-05-25 10:55:07 +0200

    Add option for private repo

--- DownloadFileGenScript.ps1
+++ DownloadFileGenScript.ps1
@@ -1,14 +1,16 @@
 ﻿$url = "https://github.com/chris514591/GenerateDummyFiles/archive/master.zip"
 $outputPath = "C:\Users\xevic\Downloads"
+# $accessToken = "YOUR_ACCESS_TOKEN"
+# Use when Folder is private
 
 # Create the output folder if it doesn't exist
 if (-not (Test-Path -Path $outputPath)) {
     New-Item -ItemType Directory -Path $outputPath | Out-Null
 }
 
-# Download the repository zip file
+# Download the repository zip file with authentication headers
 $zipFilePath = Join-Path -Path $outputPath -ChildPath "GenerateDummyFiles.zip"
-Invoke-WebRequest -Uri $url -OutFile $zipFilePath
+Invoke-WebRequest -Uri $url -OutFile $zipFilePath # Remove comment for private repo -Headers @{ "Authorization" = "Bearer $accessToken" }
 
 # Extract the contents of the zip file
 $destinationPath = Join-Path -Path $outputPath -ChildPath "GenerateDummyFiles"
