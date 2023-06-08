e0e3124b12dad4c1ffdfaf6b3c00dd5caa6bfcb8
Chris de Bruijn <xevichris@gmail.com>
2023-06-08 09:16:31 +0200

    configurebility and robustness improvements

--- DownloadFileGenScript.ps1
+++ DownloadFileGenScript.ps1
@@ -1,25 +1,30 @@
-﻿$url = "https://github.com/chris514591/GenerateDummyFiles/archive/master.zip"
-$outputPath = "C:\Users\xevic\Downloads"
-# $accessToken = "YOUR_ACCESS_TOKEN"
-# Use accessToken above when repository is private
+﻿$repositoryUrl = "https://github.com/chris514591/GenerateDummyFiles"
+# $accessToken = "YOUR_ACCESS_TOKEN" # Set this to your access token if the repository is private
+$outputPath = "C:\Users\xevic\Downloads\GenerateDummyFiles"
 
 # Create the output folder if it doesn't exist
 if (-not (Test-Path -Path $outputPath)) {
     New-Item -ItemType Directory -Path $outputPath | Out-Null
 }
 
-# Download the repository zip file with authentication headers
+# Download the repository zip file
+$zipUrl = "$repositoryUrl/archive/master.zip"
+# $headers = @{ "Authorization" = "Bearer $accessToken" }
 $zipFilePath = Join-Path -Path $outputPath -ChildPath "GenerateDummyFiles.zip"
-Invoke-WebRequest -Uri $url -OutFile $zipFilePath # >Remove comment for private repo< -Headers @{ "Authorization" = "Bearer $accessToken" }
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath -Headers $headers
 
 # Extract the contents of the zip file
 $destinationPath = Join-Path -Path $outputPath -ChildPath "GenerateDummyFiles"
 Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath -Force
 
 # Clean up the zip file
-Remove-Item -Path $zipFilePath
+Remove-Item -Path $zipFilePath -Force
+
 # Remove unnecessary files
-Remove-Item -Path "$destinationPath\GenerateDummyFiles-master\fileGen.go"
-Remove-Item -Path "$destinationPath\GenerateDummyFiles-master\go.mod"
-Remove-Item -Path "$destinationPath\GenerateDummyFiles-master\go.sum"
-Remove-Item -Path "$destinationPath\GenerateDummyFiles-master\DownloadFileGenScript.ps1"
+$filesToRemove = @("fileGen.go", "go.mod", "go.sum", "DownloadFileGenScript.ps1")
+$filesToRemove | ForEach-Object {
+    $filePath = Join-Path -Path $destinationPath -ChildPath "GenerateDummyFiles-master\$_"
+    if (Test-Path -Path $filePath) {
+        Remove-Item -Path $filePath -Force
+    }
+}
