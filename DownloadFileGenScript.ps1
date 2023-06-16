af3c4ce62bb1477368ad6b5fc0039e5919125bbf
Chris de Bruijn <xevichris@gmail.com>
2023-06-16 10:04:05 +0200

    Remove script error logging to file

--- DownloadFileGenScript.ps1
+++ DownloadFileGenScript.ps1
@@ -1,14 +1,6 @@
 ﻿$repositoryUrl = "https://github.com/chris514591/GenerateDummyFiles"
 $accessToken = "YOUR_ACCESS_TOKEN" # Set this to your access token if the repository is private
 $outputPath = "C:\Users\xevic\Downloads\GenerateDummyFiles"
-$errorLogPath = Join-Path -Path $PSScriptRoot -ChildPath "errors.log"
-
-# Function to log errors to a file
-function Write-ErrorLog {
-    param([string]$ErrorMessage)
-    Write-Host $ErrorMessage -ForegroundColor Red
-    $ErrorMessage | Out-File -FilePath $errorLogPath -Append
-}
 
 # Create the output folder if it doesn't exist
 if (-not (Test-Path -Path $outputPath)) {
@@ -16,7 +8,6 @@ if (-not (Test-Path -Path $outputPath)) {
         New-Item -ItemType Directory -Path $outputPath -ErrorAction Stop | Out-Null
     }
     catch {
-        Write-ErrorLog "Failed to create output folder: $($_.Exception.Message)"
         exit 1
     }
 }
@@ -29,7 +20,6 @@ try {
     Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath -Headers $headers -ErrorAction Stop
 }
 catch {
-    Write-ErrorLog "Failed to download repository zip file: $($_.Exception.Message)"
     exit 1
 }
 
@@ -39,7 +29,6 @@ try {
     Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath -Force -ErrorAction Stop
 }
 catch {
-    Write-ErrorLog "Failed to extract zip file: $($_.Exception.Message)"
     exit 1
 }
 
@@ -48,7 +37,6 @@ try {
     Remove-Item -Path $zipFilePath -Force -ErrorAction Stop
 }
 catch {
-    Write-ErrorLog "Failed to delete zip file: $($_.Exception.Message)"
     exit 1
 }
 
@@ -61,7 +49,6 @@ $filesToRemove | ForEach-Object {
             Remove-Item -Path $filePath -Force -ErrorAction Stop
         }
         catch {
-            Write-ErrorLog "Failed to delete file: $($_.Exception.Message)"
         }
     }
 }
